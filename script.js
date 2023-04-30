ada31353ec84fde01224226a81b2a2a708d7bcdc
Kunal-Deep011 <117732649+Kunal-Deep011@users.noreply.github.com>
2023-04-30 15:31:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-//your JS code here. If required.
-document.write("DOM load success");
+function onDOMLoad() {
+        document.body.textContent = "DOM load success";
+      }
+      document.addEventListener("DOMContentLoaded", onDOMLoad);
